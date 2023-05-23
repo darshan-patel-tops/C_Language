46786bdec42832e1348a5b11c4aeb97046a63607
darshan-patel-tops <133767986+darshan-patel-tops@users.noreply.github.com>
2023-05-23 16:12:10 +0530

    Add files via upload

---
+++ cal.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+void main(){
+	
+	int a,b;
+	printf("Enter a :");
+	scanf("%d",&a);
+	printf("Enter b :");
+	scanf("%d",&b);
+	
+	printf("Addition is :" ,(a+b));
+	printf("substraction is :",(a-b));
+	printf("multiplication is :",(a*b));
+	printf("division is : ",(a/b));
+}
---
+++ class.cpp
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+
+
+	class mobile
+	{
+		public:
+			int price=50000;
+			string mobilename;	
+			
+	};
+	
+int main()
+{
+	mobile obj;
+	
+  	MyClass myObj;  // Create an object of MyClass
+
+  	// Access attributes and set values
+  	myObj.myNum = 15; 
+  	myObj.myString = "Some text";
+
+  	// Print attribute values
+  	cout << myObj.myNum << "\n";
+  	cout << myObj.myString;
+  
+  	return 0;
+}
